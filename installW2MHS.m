b29dc252f3377f4f077b8ad0cc931b87bcc6c702
Sichao Yang <sichao@cs.wisc.edu>
2018-11-25 15:39:39 -0600

    changed variable name

--- installW2MHS.m
+++ installW2MHS.m
@@ -5,20 +5,20 @@ function installW2MHS()
     fprintf('Wisconsin White Matter Hyperintensity Segmentation and Quantification:\n');
     
     %% Add W2MHS to FilePath
-    spmtoolbox_path = fileparts(mfilename('fullpath'));
-    addpath(genpath(spmtoolbox_path));
-    fprintf('"%s" and its subdirectories successfully added to MATLAB path.\n' , spmtoolbox_path);
+    w2mhstoolbox_path = fileparts(mfilename('fullpath'));
+    addpath(genpath(w2mhstoolbox_path));
+    fprintf('"%s" and its subdirectories successfully added to MATLAB path.\n' , w2mhstoolbox_path);
 
     %% Check Training Path
     v=ver;
     if ~exist('training', 'dir')
-        error("The training folder is missing. Please download it from NITRC or SourceForge.");
+        error('The training folder is missing. Please download it from NITRC or SourceForge.');
     end
     
     %% Check Image Processing Toolbox
     v=ver;
     if ~any(strcmp({v.Name}, 'Image Processing Toolbox'))
-        error("Please install Image Processing Toolbox first.");
+        error('Please install Image Processing Toolbox first.');
     end
 
     %% Check Tools for NIfTI and ANALYZE image
@@ -26,14 +26,14 @@ function installW2MHS()
        ~exist('save_nii', 'file') || ...
        ~exist('load_nii', 'file') || ...
        ~exist('view_nii', 'file')        
-        error("Please download Tools for NIfTI and ANALYZE image and place it under the W2MHS toolbox folder.\n");
+        error('Please download Tools for NIfTI and ANALYZE image and place it under the W2MHS toolbox folder.\n');
     end
 
     %% Install Python Dependencies
     fprintf('Check and install required Python packages with pip...\n');
-    if system("python -m pip install --upgrade pip") ~= 0 || ...
-       system(sprintf("python -m pip install -r %s/requirements.txt", spmtoolbox_path)) ~= 0
-        error("Pip installation failed.");
+    if system('python -m pip install --upgrade pip') ~= 0 || ...
+       system(sprintf('python -m pip install -r %s/requirements.txt', w2mhstoolbox_path)) ~= 0
+        error('Pip installation failed.');
     end
 
     %% Done
